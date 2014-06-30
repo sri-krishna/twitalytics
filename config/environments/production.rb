b95e75c80b64c5418282aa8a6843ae1f19af003e
srikrishna <srikrishna>
2014-06-30 00:21:50 -0700

    Prepared for war deployment with pre-compiled assets

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -49,7 +49,7 @@ Twitalytics::Application.configure do
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server
   # config.action_controller.asset_host = "http://assets.example.com"
-
+  config.action_controller.asset_host = "/twitalytics"
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
   # config.assets.precompile += %w( search.js )
 
